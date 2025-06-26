b99584494a2e176512a3bb4eca0e983e7c7e1c9a
@sachind <sachind@spanidea.com>
2025-06-26 15:10:54 +0530

    final push test suite

--- tests/openwisp_test_management/apps.py
+++ tests/openwisp_test_management/apps.py
@@ -38,18 +38,18 @@ class TestManagementConfig(ApiAppConfig):
                         "name": "changelist",
                         "icon": "ow-category",
                     },
-                    # 2: {
-                    #     "label": _("Test Cases"),
-                    #     "model": get_model_name(self.label, "TestCase"),
-                    #     "name": "changelist",
-                    #     "icon": "ow-test-case",
-                    # },
-                    # 3: {
-                    #     "label": _("Test Suites"),
-                    #     "model": get_model_name(self.label, "TestSuite"),
-                    #     "name": "changelist",
-                    #     "icon": "ow-test-suite",
-                    # },
+                    2: {
+                        "label": _("Test Cases"),
+                        "model": get_model_name(self.label, "TestCase"),
+                        "name": "changelist",
+                        "icon": "ow-test-case",
+                    },
+                    3: {
+                        "label": _("Test Suites"),
+                        "model": get_model_name(self.label, "TestSuite"),
+                        "name": "changelist",
+                        "icon": "ow-test-suite",
+                    },
                     # Future items will be added here:
                     # 3: Suites
                     # 4: Mass Execution
