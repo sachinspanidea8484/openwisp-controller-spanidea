4ab74cffe3183efd31165f6d3c8c12a6d848dc9a
@ashutoshm <ashutoshm@spanidea.com>
2025-12-09 12:07:47 +0530

    fixed mqtt ssh issue

--- tests/openwisp_test_management/static/test-management/js/test_suite_execution_form.js
+++ tests/openwisp_test_management/static/test-management/js/test_suite_execution_form.js
@@ -498,6 +498,13 @@
       $("#add-device-btn").prop("disabled", false);
     }
   }
+  $(document).on("change", "input[name^='protocol_']", function () {
+    const id = $(this).attr("name").replace("protocol_", "");
+    const dev = selectedDevices.get(id);
+    if (dev) {
+      dev._chosen_protocol = $(this).val(); // Save selected protocol
+    }
+  });
 
   // Handle test suite selection change
   $(document).on("change", "#id_test_suite", function () {
