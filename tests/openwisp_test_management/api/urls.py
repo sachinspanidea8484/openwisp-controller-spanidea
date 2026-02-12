91d806b72e514f1c69a2d606edc2f6258175f757
@sachind <sachind@spanidea.com>
2026-02-12 21:55:30 +0530

    api fix exitsing

--- tests/openwisp_test_management/api/urls.py
+++ tests/openwisp_test_management/api/urls.py
@@ -216,14 +216,17 @@ urlpatterns = [
                 executor_views.get_available_devices,
                 name="api_get_available_devices",  # Made name more specific to avoid conflicts
             ),
-    # path(
-    #             "device-groups/",
-    #             executor_views.TestDeviceGroupexecutor_viewset.as_view({
-    #                 "get": "list",      # GET /device-groups/
-    #                 "post": "create"    # POST /device-groups/
-    #             }),
-    #             name="device-group-list",
-    #         ),
+
+
+    path(
+                "device-groups/",
+                executor_views.TestDeviceGroupViewSet.as_view({
+                    "get": "list",      # GET /device-groups/
+                    "post": "create"    # POST /device-groups/
+                }),
+                name="device-group-list",
+            ),
+
 
     # Un Use APIS  
     # path(
