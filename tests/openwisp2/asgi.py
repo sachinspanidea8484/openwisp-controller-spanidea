7ec1cf48bf46da3c784d6e5f9ea5b8c4fcccdd65
@sachind <sachind@spanidea.com>
2026-03-07 23:52:07 +0530

    release phase ii

--- tests/openwisp2/asgi.py
+++ tests/openwisp2/asgi.py
@@ -1,21 +1,3 @@
-# from channels.auth import AuthMiddlewareStack
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.security.websocket import AllowedHostsOriginValidator
-# from django.core.asgi import get_asgi_application
-
-# from openwisp_controller.routing import get_routes
-
-# application = ProtocolTypeRouter(
-#     {
-#         "websocket": AllowedHostsOriginValidator(
-#             AuthMiddlewareStack(URLRouter(get_routes()))
-#         ),
-#         "http": get_asgi_application(),
-#     }
-# )
-
-
-# network topology
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf import settings
